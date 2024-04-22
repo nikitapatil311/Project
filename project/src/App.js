ae02c79d60f4d42b96f6cd3255b3e4fa8f7a3ab7
Nikita S Patil <nikitaspatil311@gmail.com>
2024-04-22 15:34:20 +0200

    configured bootstrap into react project

--- project/src/App.js
+++ project/src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
 import MyCard from "../src/components/Card";
 
+// Bootstrap configuration
+
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+
 function App() {
   return (
     <div className="App">
--- project/src/components/Card.jsx
+++ project/src/components/Card.jsx
@@ -5,6 +5,10 @@ const MyCard = () => {
   return (
     <>
       <h2 className="myStyles">This is my card</h2>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+      <i class="bi bi-apple text-warning"></i>
     </>
   );
 };
