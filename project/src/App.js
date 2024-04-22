f1462d70c368633d07c4026ebad790a6436da3bb
Nikita S Patil <nikitaspatil311@gmail.com>
2024-04-22 14:57:51 +0200

    creating own component

--- project/src/App.js
+++ project/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import MyCard from "../src/components/Card";
 
 function App() {
   return (
     <div className="App">
       <h2>Welcome to Reactjs</h2>
+      <MyCard />
     </div>
   );
 }
---
+++ project/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Card.css";
+
+const MyCard = () => {
+  return (
+    <>
+      <h2 className="myStyles">This is my card</h2>
+    </>
+  );
+};
+
+export default MyCard;
