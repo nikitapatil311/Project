c93f28994c74c8987452a369e57642696391644e
Nikita S Patil <nikitaspatil311@gmail.com>
2024-04-23 18:13:32 +0200

    add usestate

--- project/src/components/gallery/gallery.jsx
+++ project/src/components/gallery/gallery.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import GalleryCard from "../gallerycard/GalleryCard";
 import img1 from "../../assests/images/img1.jpg";
 import img2 from "../../assests/images/img2.jpg";
@@ -6,22 +6,28 @@ import img3 from "../../assests/images/img3.jpg";
 import img4 from "../../assests/images/img4.jpg";
 
 const Gallery = () => {
+  let [gal, setGal] = useState([
+    { imgUrl: img1, title: "vacation" },
+    { imgUrl: img2, title: "Beach Time" },
+    { imgUrl: img3, title: "calm time" },
+    { imgUrl: img4, title: "Flying" },
+  ]);
   return (
     <>
       <div className="container mt-5">
         <div className="row">
           <div className="col-sm-3">
-            <GalleryCard image={img1} title="Vacation" />
+            <GalleryCard image={gal[0].imgUrl} title={gal[0].title} />
           </div>
 
           <div className="col-sm-3 ">
-            <GalleryCard image={img2} title="beach time" />{" "}
+            <GalleryCard image={gal[1].imgUrl} title={gal[1].title} />
           </div>
           <div className="col-sm-3">
-            <GalleryCard image={img3} title={"CalmTime"} />{" "}
+            <GalleryCard image={gal[2].imgUrl} title={gal[2].title} />
           </div>
           <div className="col-sm-3">
-            <GalleryCard image={img4} title="Flying " />{" "}
+            <GalleryCard image={gal[3].imgUrl} title={gal[3].title} />
           </div>
         </div>
       </div>
