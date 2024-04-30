06f43b511f5ef25f12f753638a5bb768f5dadc9f
Nikita S Patil <nikitaspatil311@gmail.com>
2024-04-30 12:22:34 +0200

    add nav, inc,dec,cards,usestate json stringify

--- project/src/App.js
+++ project/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-// import MyCard from "./components/card/Card";
-// import Gallery from "./components/gallery/Gallery";
+import MyCard from "./components/card/Card";
+import Gallery from "./components/gallery/Gallery";
 import Navbar from "./components/layout/navbar/Navbar";
-// import Student from "./components/student/Student";
+import Student from "./components/student/Student";
 
 // Bootstrap configuration
 
@@ -15,12 +15,11 @@ function App() {
   return (
     <div className="App">
       <Navbar header="Counter Card" color="bg-warning" expand="sm" />
+      <Student />
       <Counter />
-
-      {/* <h2>Welcome to Reactjs</h2>
+      <h2>Welcome to Reactjs</h2>
       <MyCard />
       <Gallery />
-      <Student /> */}
     </div>
   );
 }
